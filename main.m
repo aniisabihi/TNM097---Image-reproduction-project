ea3eb9b707a5223369e2e1def70673b95f843a4b
Tyddan <amanda.tyden@gmail.com>
2019-03-09 10:53:10 +0100

    skapat fler optimerade databaser

--- main.m
+++ main.m
@@ -16,9 +16,9 @@ save ImageData h lab
 load ImageData
 
 %% Mosaic
-im = imread('ImageDatabase/1.jpg'); % Bilden som ska reproduceras
+im = imread('ImageDatabase/36.jpg'); % Bilden som ska reproduceras
 [height, width, color] = size(im);
-imgSize = 1500; 
+imgSize = 1000; 
 
 if height < imgSize || width < imgSize || height > imgSize || width > imgSize
     f = warndlg('Your image does not have the desired height and width and will therefore be resized. This may affect the resemblance to the original image.','Warning');
@@ -28,59 +28,81 @@ inImg = imresize(im, [imgSize imgSize]); % 1500x1500 pixlar
 inImgLab = rgb2lab(inImg); % Konverterar frÃ¥n rgb till lab
 
 % Portrait 
-portratt = imread('TestImages/portratt.jpg');
-portratt = imresize(portratt, [1500 1500]); 
+portratt = imread('ImageDatabase/108.jpg');
+portratt = imresize(portratt, [1000 1000]); 
 portrattLab = rgb2lab(portratt);
 
 % Dark image
-mork = imread('TestImages/mork.jpg'); 
-mork = imresize(mork, [1500 1500]);
+mork = imread('ImageDatabase/1.jpg'); 
+mork = imresize(mork, [1000 1000]);
 morkLab = rgb2lab(mork);
 
 % Light image 
-ljus = imread('TestImages/ljus.jpg');
-ljus = imresize(ljus, [1500 1500]);
+ljus = imread('ImageDatabase/169.jpg');
+ljus = imresize(ljus, [1000 1000]);
 ljusLab = rgb2lab(ljus);
 
 nBlocks = 1; % Anger antalet subblocks
 tileAve = getAverages(lab,nBlocks);
 
+% 
+% %% Halverar databasen till 100 bilder
+% [hundra, hunAve] = generateData(h, tileAve, 100);
+% 
+% %% Halverar databasen till 50 bilder
+% [femtio, femAve] = generateData(h, tileAve, 50); 
 
-%% Halverar databasen till 100 bilder
-[hundra, hunAve] = generateData(h, tileAve, 100);
 
-%% Halverar databasen till 50 bilder
-[femtio, femAve] = generateData(h, tileAve, 50); 
+%% Optimering
 
-%% Halverar databasen till 25 bilder
 
-[tjugofem, tjugofemAve] = OptimizeData(lab, tileAve, morkLab); 
-
-%% Resultat
 [mosaicImg, new1]= getOptimalImages(inImg, inImgLab, h, tileAve); % Skapar mosaicbilden 
 [mosaicImg, new2]= getOptimalImages(ljus, ljusLab, h, tileAve); % Skapar mosaicbilden 
 [mosaicImg, new3]= getOptimalImages(mork, morkLab, h, tileAve); % Skapar mosaicbilden 
 [mosaicImg, new4]= getOptimalImages(portratt, portrattLab, h, tileAve); % Skapar mosaicbilden 
 
 %%
-newHam = unique(new);
+newHam = unique(new1);
 newMork = unique(new3);
 newLjus = unique(new2);
 newPortratt = unique(new4);
 
 %%
-C = intersect(newPortratt,newHam);
-B = intersect(newMork,newLjus);
+C = intersect(newLjus ,newMork);
 
-A = union(C,B);
+B = intersect(newHam  ,newPortratt);
 
-for i = 1:59
-    newDatabase60(:,:,:,i) = h(:,:,:,A(:,i));
-    newDatabase60Lab(:,:,:,i) = rgb2lab(newDatabase60(:,:,:,i));
+A = intersect(C,B);
+
+%% För att skapa nya databaser
+
+for i = 1:38
+    newDatabase38(:,:,:,i) = h(:,:,:,A(:,i));
+    newDatabase38Lab(:,:,:,i) = rgb2lab(newDatabase38(:,:,:,i));
 end
 
+%%
+% save OptimizedData38 newDatabase38 newDatabase38Lab
+
+%%
+
+load OptimizedData38
+load OptimizedData20
+load OptimizedData143
+load OptimizedData59
+%%
+
+tileAve = getAverages(newDatabase38Lab,nBlocks);
+mosaicIm = mosaic(mork, morkLab, newDatabase38, tileAve);
+
+tileAve = getAverages(newDatabase20Lab,nBlocks);
+mosaicIm = mosaic(mork, morkLab, newDatabase20, tileAve);
+
 tileAve = getAverages(newDatabase60Lab,nBlocks);
-mosaicIm = mosaic(inImg, inImgLab, newDatabase60, tileAve);
+mosaicIm = mosaic(mork, morkLab, newDatabase60, tileAve);
+
+tileAve = getAverages(newDatabase143Lab,nBlocks);
+mosaicIm = mosaic(mork, morkLab, newDatabase143, tileAve);
 
 
 % mosaicPortratt = mosaic(portratt, portrattLab, h, tileAve); % Skapar portrait mosaic
